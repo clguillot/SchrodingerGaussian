5b3859302b8378e2d87412e686e3db96a6595730
Clement Guillot <clement.guillot@enpc.fr>
2024-12-07 11:47:49 +0100

    Added Base.Threads import

--- src/SchrodingerGaussian.jl
+++ src/SchrodingerGaussian.jl
@@ -1,5 +1,7 @@
 module SchrodingerGaussian
 
+using Base.Threads
+
 using LinearAlgebra
 using BlockBandedMatrices
 using LineSearches
@@ -7,6 +9,8 @@ using ForwardDiff
 using DiffResults
 using StaticArrays
 using HermiteWavePackets
+
+
 
 include("utils.jl")
 include("block_tridiagonal_system.jl")
