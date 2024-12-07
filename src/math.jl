1bd0102f5abe5f09aa2a730fc6a8bb72ac12db65
Clement Guillot <clement.guillot@enpc.fr>
2024-12-07 15:16:27 +0100

    Fix for ForwardDiff issues

---
+++ src/math.jl
@@ -0,0 +1,24 @@
+import ForwardDiff
+import ForwardDiff: Dual
+
+# Fix for https://github.com/JuliaDiff/ForwardDiff.jl/issues/514
+function Base.exp(x::Complex{Dual{T,V,N}}) where {T,V,N}
+    xx = complex(ForwardDiff.value(real(x)), ForwardDiff.value(imag(x)))
+    dx = complex.(ForwardDiff.partials(real(x)), ForwardDiff.partials(imag(x)))
+
+    expv = exp(xx)
+    dexpv = expv * dx
+    complex(Dual{T,V,N}(real(expv), ForwardDiff.Partials{N,V}(tuple(real(dexpv)...))),
+            Dual{T,V,N}(imag(expv), ForwardDiff.Partials{N,V}(tuple(imag(dexpv)...))))
+end
+
+# Fix for https://github.com/JuliaDiff/ForwardDiff.jl/issues/514
+function Base.sqrt(x::Complex{Dual{T,V,N}}) where {T,V,N}
+    xx = complex(ForwardDiff.value(real(x)), ForwardDiff.value(imag(x)))
+    dx = complex.(ForwardDiff.partials(real(x)), ForwardDiff.partials(imag(x)))
+
+    sqrtv = sqrt(xx)
+    dsqrtv = dx / (2 * sqrtv)
+    complex(Dual{T,V,N}(real(sqrtv), ForwardDiff.Partials{N,V}(tuple(real(dsqrtv)...))),
+            Dual{T,V,N}(imag(sqrtv), ForwardDiff.Partials{N,V}(tuple(imag(dsqrtv)...))))
+end
