9aaada37bab30c9a8bf3799c9864a478abc29e94
Clement Guillot <clement.guillot@enpc.fr>
2025-01-28 17:29:16 +0100

    Test plots more informations

--- examples/schrodinger_greedy.jl
+++ examples/schrodinger_greedy.jl
@@ -3,6 +3,7 @@ using HermiteWavePackets
 using Printf
 using StaticArrays
 using Plots
+using LaTeXStrings
 
 import LinearAlgebra.BLAS
 
@@ -16,7 +17,7 @@ function test_schrodinger_greedy(a::T, b::T, Lt, nb_terms::Int, newton_nb_iter::
     try
         BLAS.set_num_threads(1)
 
-        G0 = [GaussianWavePacket1D(complex(1.0), complex(1.0), 2.0, -1.0)]
+        G0 = [GaussianWavePacket1D(complex(1.0), complex(1.0), 6.0, -1.0)]
         # G0 = [GaussianWavePacket1D(complex(0.5), complex(8.0), 1/sqrt(2.0), 0.0)]
         
         Gf = zeros(GT, 0, Lt)
@@ -25,14 +26,14 @@ function test_schrodinger_greedy(a::T, b::T, Lt, nb_terms::Int, newton_nb_iter::
         #     Gf[1, k] = Gaussian{T}(0.5*exp(t), 1.0, 5.0, -1.0)
         # end
 
-        Gv = Gaussian1D(2.0, 1.0, 0.0)
+        Gv = Gaussian1D(1.0, 1.0, 0.0)
         v(x) = Gv(x)
         # v(x) = x^4 - x^2
 
-        G_list = schrodinger_gaussian_greedy(a, b, Lt, G0, apply_op, Gf, nb_terms; maxiter=newton_nb_iter, verbose=true)
+        G_list, res_list = schrodinger_gaussian_greedy(a, b, Lt, G0, apply_op, Gf, nb_terms; maxiter=newton_nb_iter, verbose=true)
 
         if plot_resut
-            x_list = T.(-3:0.02:3)
+            x_list = T.(-10:0.02:10)
             t_list = zeros(Lt)
             norm_list = zeros(Lt)
             g = @gif for k in 1:Lt
@@ -48,11 +49,32 @@ function test_schrodinger_greedy(a::T, b::T, Lt, nb_terms::Int, newton_nb_iter::
                 # fx_re = real.(fgx)
                 # fx_im = imag.(fgx)
                 fx_v = v.(x_list)
-                plot(x_list, [fx, fx_v], legend=:none, ylims=(-0.4, 1.2))
+                plot(x_list, [fx, fx_v], legend=:none, ylims=(-0.4, 2.0))
             end fps=30 every (max(round(Int, Lt / (30 * (b-a))), 1))
-
             display(g)
-            display(plot(norm_list; label="L2 Norm"))
+
+            println("res_list = ", res_list)
+
+            p = plot()
+            for k in [1, Lt]
+                t = a + (k-1) * (b-a)/(Lt-1)
+                G = zeros(GT, nb_terms)
+                for j=1:nb_terms
+                    G[j] = inv_fourier(unitary_product(2*t, fourier(G_list[j, k])))
+                end
+                fgx = G.(x_list)
+                fx = abs2.(fgx)
+                plot!(p, x_list, fx; label=LaTeXString("\$|ψ($(round(Int, t)), x)|^2\$"), ylims=(-0.4, 2.0))
+            end
+            plot!(p, x_list, v.(x_list); label=LaTeXString("\$v(x)\$"))
+            display(p)
+            # savefig("state.pdf")
+
+            p = plot()
+            plot!(p, t_list, norm_list; label=LaTeXString("\$ \\Vert ψ(t) \\Vert _{L^2}\$"))
+            plot!(p, t_list, fill(norm_L2(G0[1]), Lt); label=LaTeXString("\$ \\Vert g_0 \\Vert _{L^2}\$"))
+            display(p)
+            # savefig("norm.pdf")
         end
 
         println("Test application :")
