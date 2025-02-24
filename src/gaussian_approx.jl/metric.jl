93fc749afad7e51a97b2cf2c5eb1bb04a0f822b5
Clement Guillot <clement.guillot@enpc.fr>
2025-02-24 11:02:56 +0100

    Moved unpacking outside of gaussian_approx

--- src/gaussian_approx.jl/metric.jl
+++ src/gaussian_approx.jl/metric.jl
@@ -2,14 +2,7 @@
 #=
     Returns real(dot_L2(G_X1, G_X2))
 =#
-function gaussian_approx_metric(X1::AbstractVector{T1}, X2::AbstractVector{T2}, ::Val{check_len}=Val(true)) where{T1<:Real, T2<:Real, check_len}
-    if check_len && (length(X1) != gaussian_param_size || length(X2) != gaussian_param_size)
-        throw(DimensionMismatch("X1 and X2 must be vectors of size $gaussian_param_size"))
-    end
-
-    G1 = unpack_gaussian_parameters(X1)
-    G2 = unpack_gaussian_parameters(X2)
-
+function gaussian_approx_metric(G1::AbstractWavePacket, G2::AbstractWavePacket)
     return real(dot_L2(G1, G2))
 end
 
@@ -48,7 +41,11 @@ function gaussian_approx_metric_topright_hessian!(Htr::Matrix{T}, X1::Vector{T},
         throw(DimensionMismatch("X2 must be a vector of size $gaussian_param_size but has size $(length(X2))"))
     end
 
-    f(Y1, Y2) = gaussian_approx_metric(Y1, Y2, Val(false))
+    function f(Y1, Y2)
+        G1 = unpack_gaussian_parameters(Y1)
+        G2 = unpack_gaussian_parameters(Y2)
+        return gaussian_approx_metric(G1, G2)
+    end
     ∇₁f!(∇, Y1, Y2) = ForwardDiff.gradient!(∇, Z -> f(Z, Y2), Y1, cfg.gradient_cfg, Val(false))
     ForwardDiff.jacobian!(Htr, (∇, Z) -> ∇₁f!(∇, X1, Z), cfg.W, 
         X2, cfg.jacobian_cfg, Val(false))
