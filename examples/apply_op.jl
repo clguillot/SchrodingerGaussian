03c216b67436c49e1e457e86818af7530cd1e1d4
Clement Guillot <clement.guillot@enpc.fr>
2025-02-25 10:55:11 +0100

    Defined apply_op for multidim

--- examples/apply_op.jl
+++ examples/apply_op.jl
@@ -1,7 +1,8 @@
 using HermiteWavePackets
 using StaticArrays
 
-function apply_op(t::T, G::Gtype) where{T<:Real, Gtype<:AbstractWavePacket1D}
+# 1D operator
+function apply_op(t::T, G::AbstractWavePacket1D) where{T<:Real}
     G1 = inv_fourier(unitary_product(2*t, fourier(G)))
     Gv = GaussianWavePacket1D(1.0, 1.0, 0.0, 0.0)
     G2 = Gv * G1
@@ -16,4 +17,15 @@ function apply_op(t::T, G::Gtype) where{T<:Real, Gtype<:AbstractWavePacket1D}
     # H1 = inv_fourier(unitary_product(-2*t, fourier(Gv1 * G1)))
     # H2 = inv_fourier(unitary_product(-2*t, fourier(Gv2 * G1)))
     # return SVector(H1, H2)
+end
+
+# Generalized operator
+function apply_op(t::T, G::GaussianWavePacket{D}) where{T<:Real, D}
+    # Potential barrier
+    Gv = GaussianWavePacket(1.0, (@SVector fill(1.0, D)), (@SVector zeros(D)), (@SVector zeros(D)))
+
+    u = @SVector fill(2*t, D)
+    G1 = inv_fourier(unitary_product(u, fourier(G)))
+    G2 = Gv * G1
+    return inv_fourier(unitary_product(-u, fourier(G2)))
 end
