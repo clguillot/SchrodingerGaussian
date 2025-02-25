438063bc5a94332b798de601cd82f70adf2124e2
Clement Guillot <clement.guillot@enpc.fr>
2025-02-25 09:49:27 +0100

    Switched test

--- examples/apply_op.jl
+++ examples/apply_op.jl
@@ -5,7 +5,7 @@ function apply_op(t::T, G::Gtype) where{T<:Real, Gtype<:AbstractWavePacket1D}
     G1 = inv_fourier(unitary_product(2*t, fourier(G)))
     Gv = GaussianWavePacket1D(1.0, 1.0, 0.0, 0.0)
     G2 = Gv * G1
-    # return inv_fourier(unitary_product(-2*t, fourier(G2)))
+    return inv_fourier(unitary_product(-2*t, fourier(G2)))
     # P = SVector(zero(T), zero(T), -one(T), zero(T), one(T))
     # G2 = polynomial_product(zero(T), P, HermiteWavePacket1D(G1))
     # return inv_fourier(unitary_product(-2*t, fourier(G2)))
