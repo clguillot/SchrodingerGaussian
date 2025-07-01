7db676bc110452e9d006d657622fd580ab8977a1
Clement Guillot <clement.guillot@enpc.fr>
2025-07-01 10:44:29 +0200

    conflict

--- examples/schrodinger_greedy.jl
+++ examples/schrodinger_greedy.jl
@@ -9,45 +9,34 @@ import LinearAlgebra.BLAS
 
 include("reference_solution.jl")
 
-function apply_op_1D(t, G)
-    G1 = inv_fourier(unitary_product(fourier(G), SVector(2*t)))
-    Gv1 = Gaussian(1.0, 1.0, -2.0)
-    Gv2 = Gaussian(0.0, 1.0, 2.0)
-    G2 = Gv1 * G1 + Gv2 * G1
-    return inv_fourier(unitary_product(fourier(G2), SVector(-2*t)))
-end
+function plot_schrodinger_greedy(dis::Discretization, pot::Potential; plot_result=true) 
 
-function test_schrodinger_greedy(a::T, b::T, Lt, nb_terms::Int, newton_nb_iter::Int, ::Type{T}, plot_resut) where{T<:AbstractFloat}
+    Gtype = typeof(dis.G0)
+    T = typeof(dis.t0)
 
-    Gtype = GaussianWavePacket{1, Complex{T}, Complex{T}, T, T}
+    pot = Potential([2.5,0.5],[1.0,1.0], [-3.,3.])
 
-    G0 = GaussianWavePacket(complex(1.0), complex(1.0), 5.0, -1.0)
-    G0 = G0 / norm_L2(G0)
-
-    Gv = Gaussian(1.0, 1.0, -2.0) + Gaussian(1.0, 1.0, 2.0)
-    v(x) = Gv(x)
-
-    G_list, res_list = schrodinger_gaussian_greedy(Gtype, T, a, b, Lt, G0, apply_op_1D, nb_terms;greedy_orthogonal=false, maxiter=newton_nb_iter, verbose=true, fullverbose=false)
+    G_list, res_list = schrodinger_gaussian_greedy(dis,pot)
 
     # M = 30.0
     # Lx = 4096
     # U = schrodinger_sine(a, b, Lt, WavePacketArray(G0), v, M, Lx)
 
-    if plot_resut
+    if plot_result
         x_list = T.(-10:0.02:10)
-        t_list = zeros(Lt)
-        norm_list = zeros(Lt)
-        g = @gif for k in 1:Lt
-            t = a + (k-1) * (b-a)/(Lt-1)
-            G = WavePacketSum(zeros(Gtype, nb_terms))
-            for j in 1:nb_terms
+        t_list = zeros(dis.Nt)
+        norm_list = zeros(dis.Nt)
+        g = @gif for k in 1:dis.Nt
+            t = dis.t0 + (k-1) * (dis.tf-dis.t0)/(dis.Nt-1)
+            G = WavePacketSum(zeros(Gtype, dis.nb_g))
+            for j in 1:dis.nb_g
                 G.g[j] = inv_fourier(unitary_product(fourier(G_list[j, k]), SVector(2*t)))
             end
             t_list[k] = t
             norm_list[k] = norm_L2(G)
             fgx = G.(x_list)
             fx = abs2.(fgx)
-            fx_v = v.(x_list)
+            fx_v = pot.(x_list)
 
             # f_ref = zeros(length(x_list))
             # for j in eachindex(x_list)
@@ -59,14 +48,14 @@ function test_schrodinger_greedy(a::T, b::T, Lt, nb_terms::Int, newton_nb_iter::
             #     f_ref[j] = abs2(μ)
             # end
             plot(x_list, [fx, fx_v#=, f_ref=#], legend=:none, ylims=(-0.4, 2.0))
-        end fps=30 every (max(round(Int, Lt / (30 * (b-a))), 1))
+        end fps=30 every (max(round(Int, dis.Nt / (30 * (dis.tf-dis.t0))), 1))
         display(g)
 
         println("res_list = ", res_list)
 
         p = plot()
         plot!(p, t_list, norm_list; label=LaTeXString("\$ \\Vert ψ(t) \\Vert _{L^2}\$"))
-        plot!(p, t_list, fill(norm_L2(G0), Lt); label=LaTeXString("\$ \\Vert g_0 \\Vert _{L^2}\$"))
+        plot!(p, t_list, fill(norm_L2(G0), dis.Nt); label=LaTeXString("\$ \\Vert g_0 \\Vert _{L^2}\$"))
         display(p)
         # savefig("norm.pdf")
 
@@ -86,3 +75,9 @@ function test_schrodinger_greedy(a::T, b::T, Lt, nb_terms::Int, newton_nb_iter::
         # savefig("state.pdf")
     end
 end
+
+G0 = GaussianWavePacket(complex(1.0),complex(1.0),6.,-1.0)
+G0 = G0 / norm_L2(G0)
+dis = Discretization(0.,5.,50,G0,10,100,false)
+pot = Potential([1.5,1.0],[1.0,1.0],[-2.0,2.0])
+plot_schrodinger_greedy(dis,pot)
